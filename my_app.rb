f1642aa9edccaea686b573dfe5010187861b7ce1
Mark Wotton <mwotton@gmail.com>
2011-06-15 09:39:02 +1000

    add caching

--- my_app.rb
+++ my_app.rb
@@ -12,6 +12,7 @@ end
 
 get '/cache' do
   raise 401 unless params[:key] == 'secret_kod_key'
+  headers['Cache-Control'] = 'public, max-age=604800'
   url = CGI::unescape(params[:url])
   HTTParty.get(url)
 end
